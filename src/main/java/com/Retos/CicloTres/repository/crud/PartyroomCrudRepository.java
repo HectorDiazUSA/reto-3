e3c18568c42d34da1ff39b2238187d205e2f185c
HectorDiazUSA <93656052+HectorDiazUSA@users.noreply.github.com>
2021-11-18 10:40:26 -0500

    Delete src directory

--- src/main/java/com/Retos/CicloTres/repository/crud/PartyroomCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Retos.CicloTres.repository.crud;
-
-import com.Retos.CicloTres.model.Partyroom;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PartyroomCrudRepository extends CrudRepository<Partyroom, Integer>{
-    
-}
