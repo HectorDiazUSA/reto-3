83d81fdb413d139fec2a5acafc3e65a3555779ba
hector.delgado.mt@correo.usa.edu.co <hector.delgado.mt@correo.usa.edu.co>
2021-11-18 10:43:30 -0500

    se actualiza el archivo de funciones

---
+++ src/main/java/com/Retos/CicloTres/repository/crud/PartyroomCrudRepository.java
@@ -0,0 +1,8 @@
+package com.Retos.CicloTres.repository.crud;
+
+import com.Retos.CicloTres.model.Partyroom;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartyroomCrudRepository extends CrudRepository<Partyroom, Integer>{
+    
+}
